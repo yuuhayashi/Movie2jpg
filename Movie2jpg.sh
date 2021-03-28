7a0472148b1d0a207908b87a0521422486c1a7f6
haya4 <hayashi.yuu@gmail.com>
2021-03-28 15:22:04 +0900

    sh update

--- Movie2jpg.sh
+++ Movie2jpg.sh
@@ -1,2 +1,1 @@
-cd ~/docker/Movie2jpg-master
-docker run -it -v $(pwd):/mnt/mapi:rw haya4/movie2jpg java -cp .:/root/Movie2jpg-2.0.0-jar-with-dependencies.jar osm.surveyor.movie2jpg.Movie2jpg /mnt/mapi
+docker run -it -v $(pwd):/mnt/mapi:rw haya4/movie2jpg:2.0.0 java -cp .:/root/Movie2jpg-2.0.0-jar-with-dependencies.jar osm.surveyor.movie2jpg.Movie2jpg /mnt/mapi
